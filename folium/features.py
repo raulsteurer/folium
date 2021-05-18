834ba8f79d4e19bbb57f085529f266b4a05d0308
Raul Steurer <raul.steurer@gmail.com>
2021-05-18 22:32:29 +0200

    Update features.py

--- folium/features.py
+++ folium/features.py
@@ -1381,6 +1381,33 @@ class LatLngPopup(MacroElement):
     def __init__(self):
         super(LatLngPopup, self).__init__()
         self._name = 'LatLngPopup'
+        
+        
+class LatLngPopupClickable(MacroElement):
+    """
+    When one clicks on a Map that contains a LatLngPopupClickable,
+    a popup is shown that displays the latitude and longitude of the pointer.
+
+    """
+    _template = Template(u"""
+            {% macro script(this, kwargs) %}
+                var {{this.get_name()}} = L.popup();
+                function latLngPop(e) {
+                data = e.latlng.lat.toFixed(4) + "," + e.latlng.lng.toFixed(4);
+                    {{this.get_name()}}
+                        .setLatLng(e.latlng)
+                        .setContent( "<br /><a href="+data+"> click </a>")
+                        .openOn({{this._parent.get_name()}})
+                    }
+                {{this._parent.get_name()}}.on('click', latLngPop);
+
+            {% endmacro %}
+            """)  # noqa
+
+    def __init__(self):
+        super(LatLngPopupClickable, self).__init__()
+        self._name = 'LatLngPopupClickable'
+
 
 
 class ClickForMarker(MacroElement):
